bed283f6d8446f15c356fe208d3018f67ccbe47b
donnasaud <donnaalmadani@gmail.com>
2025-11-15 14:00:48 +0000

    advanced task 100

---
+++ python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Duck typing - first element of a sequence"""
+from typing import Any, Sequence, Optional
+
+def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
+    """Return the first element of a sequence if it exists, else None"""
+    if lst:
+        return lst[0]
+    return None
+
